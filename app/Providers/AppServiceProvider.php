24ce8c66ec6d4cba687a1e2d9fab284dbd557336
Glenvaldi Felix Tantoko <glenvaldi@gmail.com>
2026-04-12 22:56:53 +0700

    Fix mixed content https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,25 +1,17 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL; // <-- Tambahin ini di paling atas
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void { }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        // Paksa semua link jadi HTTPS kalau di server Railway
         if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
